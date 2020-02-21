364341ef4df69e2942e764e09fea90a4ae9d5b43
rabbeh <rajathebbar@gmail.com>
2020-02-20 22:18:54 -0800

    Removed certificates

--- flask_app.py
+++ flask_app.py
@@ -5,11 +5,9 @@ from rq import Connection, Worker
 from speaking_time import app
 
 manager = Manager(app)
-#context = ('/home/sail1/rajatheb/public_html/moderator_controlled_speaking_time/ssl/server.crt', '/home/sail1/rajatheb/public_html/moderator_controlled_speaking_time/ssl/server.key')
-context = ('/home/sail3/web/security_certs/sail_usc_edu_cert.cer','/home/sail3/web/security_certs/sail.key')
 manager.add_command(
     'runserver',
-    Server(port=1030, use_debugger=True, use_reloader=True, ssl_crt=context[0], ssl_key=context[1], threaded=True))
+    Server(port=1030, use_debugger=True, use_reloader=True, threaded=True))
 
 
 @manager.command
